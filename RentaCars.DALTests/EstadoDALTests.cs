9ef274c43998578a4254a39aaba2612d8ef16f1d
Pedro Cano <47805199+PAC97@users.noreply.github.com>
2019-05-05 09:11:06 -0600

    Pruebas CRUD de modulo Estado Completo

---
+++ RentaCars.DALTests/EstadoDALTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RentaCars.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentaCars.EN;
+
+namespace RentaCars.DAL.Tests
+{
+    [TestClass()]
+    public class EstadoDALTests
+    {
+        EstadoDAL EDAL = new EstadoDAL();
+        [TestMethod()]
+        public void AddEstadoTest()
+        {
+            Estado es = new Estado { Nombre = "Pendiente" };
+            int r = EDAL.AddEstado(es);
+            List<Estado> estados = EDAL.StateList();
+            Estado es1 = estados.OrderByDescending(s => s.EstadoID).FirstOrDefault();
+            Assert.IsTrue(r > 0 && es.Nombre == es1.Nombre);
+        }
+
+        [TestMethod()]
+        public void StateListTest()
+        {
+            EDAL.AddEstado(new Estado { Nombre = "Aprobado" });
+            List<Estado> estados = EDAL.StateList();
+            Console.WriteLine("Datos encontrados en la base de datos {0}", estados.Count);
+            Assert.IsTrue(estados.Count > 0);
+        }
+
+        [TestMethod()]
+        public void UpdateStateTest()
+        {
+            EDAL.AddEstado(new Estado { Nombre = "Aprobado" });
+            List<Estado> estados = EDAL.StateList();
+            Estado es = estados.OrderByDescending(s => s.EstadoID).FirstOrDefault();
+            es.Nombre = "Si aprobado";
+            int r = EDAL.UpdateState(es);
+            Assert.IsTrue(r > 0);
+        }
+
+        [TestMethod()]
+        public void FindStateTest()
+        {
+            EDAL.AddEstado(new Estado { Nombre = "Rquerido" });
+            List<Estado> estados = EDAL.StateList();
+            Estado es = estados.OrderByDescending(s => s.EstadoID).FirstOrDefault();
+            Estado esID = EDAL.FindState(es.EstadoID);
+            Assert.IsTrue(esID.EstadoID > 0 && es.Nombre == esID.Nombre);
+        }
+    }
+}
---
+++ RentaCarsPruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RentaCarsPruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RentaCarsPruebas")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c186e08-e496-4e6b-9210-b5c110bc3e54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
